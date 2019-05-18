afb1cbd4bc34befeb80c383d6ad74d5092c7d421
Kumiko Nomura <kumiko.nomura@laverne.edu>
2019-05-18 14:05:27 -0700

    set up server with handlebars and serving public directory

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-console.log('file working')
+const express = require('express')
+const exphbs = require('express-handlebars')
+const { join } = require('path')
+const server = express()
+
+// Serve static content for the app from the "public" directory in the application directory.
+server.use(express.static(join(__dirname, 'public')))
+
+// Parse application body
+// urlencoded lets you utilize nested json
+server.use(express.urlencoded({ extended: true }))
+server.use(express.json())
+
+server.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+// specifying the engine where we will be doing the server side rendering
+server.set('view engine', 'handlebars')
+
+server.listen(3000, () => {
+  console.log('server listening on port: 3000')
+})
