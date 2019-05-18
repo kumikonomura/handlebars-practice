439dbcd40b8de82895646f6ccf1cd5cdb931a856
Kumiko Nomura <kumiko.nomura@laverne.edu>
2019-05-18 14:41:21 -0700

    initialized views

--- routes/milkshakes.js
+++ routes/milkshakes.js
@@ -4,7 +4,11 @@ const { Router } = require('express')
 const router = Router()
 
 router.get('/', (req, res) => {
-  
+  return res.render('index')
+})
+
+router.get('/milkshakes', (req, res) => {
+  return res.render('milkshakes')
 })
 
 module.exports = router
